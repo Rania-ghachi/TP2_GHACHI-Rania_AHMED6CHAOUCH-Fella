0f8cb5052d0acee3893a54699c0a1b8cf71ebdf4
rania ra <rina.ra.1804@gmail.com>
2021-03-06 20:17:12 +0100

    Modification de l'interface InterfaceEtudiantService

--- src/InterfaceEtudiantService.java
+++ src/InterfaceEtudiantService.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public interface InterfaceEtudiantService {
+
+public boolean inscription (InterfaceEtudiant etud);
+	
+	public ArrayList<InterfaceEtudiant> GetEtudiantParUniversitye();
+	
+	public ArrayList<InterfaceEtudiant> GetEtudiatparLivreEmprunte();
+
+	
+
 
 }
